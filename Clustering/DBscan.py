c7895714180ee71e79f3217f7d11d475320ef7a5
Nickson <zz20102aa@gmail.com>
2023-03-20 11:02:41 +0000

    finsih dbscsan

--- Clustering/DBscan.py
+++ Clustering/DBscan.py
@@ -4,48 +4,84 @@ import numpy as np
 import random
 import time
 import math
-visit=[]
-radius=100.
-min_density=15
-def calculate_dis(x1,y1,x2,y2):
-    return math.sqrt((x1-x2)**2+(y1-y2)**2)
-def find_candidate(candidate_idx,all_distance,target_idx):
-    target=all_distance[target_idx]
-    neighbors_idx=np.where(target<=radius)
-    if(np.size(neighbors_idx)>=min_density):
-        candidate_idx.append(target_idx) 
-        print(neighbors_idx)
-        for idx in neighbors_idx:
-            find_candidate(candidate_idx,all_distance,idx)
-    return candidate_idx
-if __name__ == "__main__":
-    x=[]
-    y=[]
-    random.seed(time.time())
-    with open("Clustering_test1") as file:
-        for line in file:
-            data=line.split(' ')
-            x.append(float(data[0]))
-            y.append(float(data[1]))
-    x=np.array(x)
-    y=np.array(y)
-    total=x.shape[0]
-    all_distance=np.zeros(shape=(total,total))
-    for i in range(total):
-        for j in range(total):
-            if(i==j): continue
-            distance=calculate_dis(x[i],y[i],x[j],y[j])
-            all_distance[i][j]=distance
-    #target_idx=random.randint(0,total)
-    while True:
-        if(np.all(visit==1)):
-            break
-        target_idx=480
-        candidate_idx=[]
-        find_candidate(candidate_idx,all_distance,target_idx)
-        print(candidate_idx)
-        break
-            
+radius = [6.0, 5.0, 4.4, 3.5, 3.6]
+min_density = [15, 13, 14, 13, 14]
+c = 1
 
-    # plt.scatter(x,y)
-    # plt.show()
+
+def calculate_dis(x1, y1, x2, y2):
+    return math.sqrt((x1-x2)**2+(y1-y2)**2)
+
+
+def find_candidate(candidate_idx, all_distance, target_idx, visit):
+    candidate_idx.append(target_idx)
+    visit[target_idx] = 1
+    target = all_distance[target_idx]
+    neighbors_idx = np.where(target <= radius[c-1])[0]
+    if (np.size(neighbors_idx) >= min_density[c-1]):
+        for idx in neighbors_idx:
+            if (visit[idx] == 0):
+                find_candidate(candidate_idx, all_distance, idx, visit)
+    return candidate_idx, visit
+
+
+if __name__ == "__main__":
+
+    path = "./Clustering_test"
+    while (os.path.exists(f"./{path}{c}")):
+        x = []
+        y = []
+        with open(f"./{path}{c}") as file:
+            for line in file:
+                data = line.split(' ')
+                x.append(float(data[0]))
+                y.append(float(data[1]))
+        x = np.array(x)
+        y = np.array(y)
+        total = x.shape[0]
+        all_distance = np.zeros(shape=(total, total))
+        for i in range(total):
+            for j in range(total):
+                if (i == j):
+                    continue
+                distance = calculate_dis(x[i], y[i], x[j], y[j])
+                all_distance[i][j] = distance
+        # target_idx=random.randint(0,total)
+        visit = np.zeros(total)
+
+        all_point_neighbors = []
+        for point in all_distance:
+            target_idx = np.where(point < radius[c-1])[0]
+            neighbors_size = np.size(target_idx)
+            all_point_neighbors.append(neighbors_size)
+
+        # every time the start point is from central point
+        all_point_neighbors = np.array(all_point_neighbors)
+        max_neighbors_idx = np.argmax(all_point_neighbors)
+        dataset_id = np.ndarray(total)
+        cluster_id = 1
+        while True:
+            if (np.all(visit == 1)):
+                break
+            candidate_idx = []
+            candidate_idx, visit = find_candidate(
+                candidate_idx, all_distance, max_neighbors_idx, visit)
+
+            # find another central point
+            visit_idx = np.where(visit == 1)[0]
+            all_point_neighbors[visit_idx] = 0
+            max_neighbors_idx = np.argmax(all_point_neighbors)
+
+            # recorder the candidate cluster id
+
+            dataset_id[candidate_idx] = cluster_id
+            plt.scatter(x[candidate_idx], y[candidate_idx])
+            cluster_id += 1
+
+        # save the result
+        with open(f"output_{c}.txt", "w") as outfile:
+            for i in range(total):
+                outfile.write(f"{x[i]} {y[i]} {dataset_id[i]}\n")
+        plt.savefig(f"Cluster_{c}.jpg")
+        plt.clf()
+        c += 1
