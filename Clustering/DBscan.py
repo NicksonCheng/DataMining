bdf824e39b86e66b19e0a2de183cd0b910f366d8
Nickson <44738168+NicksonCheng@users.noreply.github.com>
2023-03-20 15:47:27 +0800

    recursive got memory problem

--- Clustering/DBscan.py
+++ Clustering/DBscan.py
@@ -4,9 +4,20 @@ import numpy as np
 import random
 import time
 import math
+visit=[]
+radius=100.
+min_density=15
 def calculate_dis(x1,y1,x2,y2):
     return math.sqrt((x1-x2)**2+(y1-y2)**2)
-
+def find_candidate(candidate_idx,all_distance,target_idx):
+    target=all_distance[target_idx]
+    neighbors_idx=np.where(target<=radius)
+    if(np.size(neighbors_idx)>=min_density):
+        candidate_idx.append(target_idx) 
+        print(neighbors_idx)
+        for idx in neighbors_idx:
+            find_candidate(candidate_idx,all_distance,idx)
+    return candidate_idx
 if __name__ == "__main__":
     x=[]
     y=[]
@@ -19,23 +30,22 @@ if __name__ == "__main__":
     x=np.array(x)
     y=np.array(y)
     total=x.shape[0]
-    radius=30
-    min_density=15
     all_distance=np.zeros(shape=(total,total))
     for i in range(total):
         for j in range(total):
             if(i==j): continue
             distance=calculate_dis(x[i],y[i],x[j],y[j])
             all_distance[i][j]=distance
-    all_distance=np.sort(all_distance,axis=0)
     #target_idx=random.randint(0,total)
-    visit=np.zeros(total)
-    count=0
-    while count<total:    
+    while True:
+        if(np.all(visit==1)):
+            break
         target_idx=480
-        target=all_distance[target_idx]
-        neighbors_idx=np.where(target<=radius)
-        neighbors=target[neighbors_idx]
+        candidate_idx=[]
+        find_candidate(candidate_idx,all_distance,target_idx)
+        print(candidate_idx)
+        break
+            
 
     # plt.scatter(x,y)
     # plt.show()
